8533e3c9e17852b027f8474e28d737ca6c24dfbc
lcolladotor <lcollado@jhsph.edu>
2015-04-14 21:13:43 -0400

    Import Shula data from BED files
    This script imports data from BED files and creates the fullCov object needed
    for derfinder analyses.

---
+++ CoverageInfo/import-data.R
@@ -0,0 +1,55 @@
+## Generate fullCov object from BED files
+
+library('rtracklayer')
+library('BiocParallel')
+library('devtools')
+library('BSgenome.Hsapiens.UCSC.hg19')
+
+## Define files
+beds <- dir('/dcs01/ajaffe/ChIPseq/Shulha2013/BED', pattern = 'c', full.names = TRUE)
+names(beds) <- dir('/dcs01/ajaffe/ChIPseq/Shulha2013/BED', pattern = 'c')
+
+## Load data for all chrs per file
+system.time(
+    bedGR <- bplapply(beds, function(bed) {
+        library('rtracklayer')
+        b <- BEDFile(bed)
+        import(b)
+    }, BPPARAM = SnowParam(workers = 10))
+)
+print(object.size(bedGR), units = 'Gb')
+
+## Set the chr lengths
+bedGR <- lapply(bedGR, function(gr) {
+    seqs <- names(seqlengths(gr))
+    match.names <- match(seqs, names(seqlengths(BSgenome.Hsapiens.UCSC.hg19)))
+    match.names <- match.names[!is.na(match.names)]
+    seqlengths(gr) <- seqlengths(BSgenome.Hsapiens.UCSC.hg19)[match.names]
+    return(gr)
+})
+
+## Calculate coverage
+system.time(
+    bedCov <- bplapply(bedGR, function(bed) {
+        library('GenomicRanges')
+        coverage(bed)
+    }, BPPARAM = SnowParam(workers = 10))
+)
+print(object.size(bedCov), units = 'Gb')
+
+## Build fullCov object
+chrs <- paste0('chr', c(1:22, 'X', 'Y', 'M'))
+system.time(
+    fullCov <- lapply(chrs, function(chr) {
+        DataFrame(lapply(bedCov, '[[', chr))
+    })
+)
+names(fullCov) <- chrs
+
+## Save the coverage data
+save(fullCov, file = 'fullCov.Rdata')
+
+## Reproducibility
+proc.time()
+options(width = 120)
+session_info()
