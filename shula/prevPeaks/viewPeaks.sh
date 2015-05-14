4305678a71b2bfc1498051c90ad89f9506e7acd8
lcolladotor <lcollado@jhsph.edu>
2015-05-14 11:55:49 -0400

    fix a typo, save log files in logs dir

--- shula/prevPeaks/viewPeaks.sh
+++ shula/prevPeaks/viewPeaks.sh
@@ -2,13 +2,17 @@
 #$ -cwd
 #$ -m e
 #$ -l mem_free=30G,h_vmem=100G
-#$ -N viewPeaks
+#$ -N visPeaks
 echo "**** Job starts ****"
 date
 
+mkdir -p logs
+
 # Generate HTML
 module load R/3.1.x
-Rscript -e "library(rmarkdown); render('viewPeaks', clean = FALSE)"
+Rscript -e "library(rmarkdown); render('viewPeaks.Rmd', clean = FALSE)"
+
+mv visPeaks.* logs/
 
 echo "**** Job ends ****"
 date
