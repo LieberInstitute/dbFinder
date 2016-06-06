4314dd690f3821814b12540e6ce43fddcdb489ce
lcolladotor <lcollado@jhsph.edu>
2016-06-06 01:54:26 -0400

    Already process results

--- step6b-regionMatrix-merge.R
+++ step6b-regionMatrix-merge.R
@@ -26,6 +26,7 @@ cutoff <- opt$cutoff
 ## Load data
 chrs <- paste0('chr', c(1:22, 'X', 'Y'))
 regionMat <- lapply(chrs, function(chr) {
+    message(paste(Sys.time(), 'processing', chr))
     load(paste0('regionMat-', opt$histone, '-cut', cutoff, '-', chr, '.Rdata'))
     res <- regionMat
     return(res)
@@ -35,7 +36,22 @@ regionMat <- lapply(chrs, function(chr) {
 regionMat <- do.call(c, regionMat)
 
 ## Save
+message(paste(Sys.time(), 'saving full results'))
 save(regionMat, file = paste0('regionMat-', opt$histone, '-cut', cutoff, '.Rdata'))
+
+## Extract info
+message(paste(Sys.time(), 'extracting regions'))
+fullRegions  <- unlist(GRangesList(lapply(regionMat, '[[', 'regions')))
+names(fullRegions) = NULL
+
+message(paste(Sys.time(), 'saving regions'))
+save(fullRegions, file = paste0('fullRegions-', opt$histone, '-cut', cutoff, '.Rdata'))
+
+message(paste(Sys.time(), 'extracting coverage matrix'))
+coverageMatrix <- do.call("rbind", lapply(regionMat, '[[', 'coverageMatrix'))
+
+message(paste(Sys.time(), 'saving coverage matrix'))
+save(coverageMatrix, file = paste0('coverageMatrix-', opt$histone, '-cut', cutoff, '.Rdata'))
 
 ## Reproducibility info
 proc.time()
