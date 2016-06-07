a7bd430f35d502ddf512416bb53f26a38aaf6841
lcolladotor <lcollado@jhsph.edu>
2016-06-06 21:53:00 -0400

    Temporarely put it on hold for sra.rse

--- step6-regionMatrix.sh
+++ step6-regionMatrix.sh
@@ -37,7 +37,7 @@ do
 #$ -m e
 #$ -l mem_free=70G,h_vmem=90G,h_fsize=30G
 #$ -N ${sname}
-#$ -hold_jid fullCov-${EXPERIMENT}
+#$ -hold_jid fullCov-${EXPERIMENT},sra.rse
 #$ -t 1:24
 
 echo "**** Job starts ****"
