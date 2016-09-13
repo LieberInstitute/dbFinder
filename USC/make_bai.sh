f44aa963fdbc1cd805ec9b98920d955fa30360e9
lcolladotor <lcollado@jhsph.edu>
2016-09-13 15:14:02 -0400

    Add script for creating .bam.bai files for USC data

---
+++ USC/make_bai.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -cwd
+#$ -m e
+#$ -N makeBai-USC
+#$ -pe local 10
+
+module load samtools/1.1
+
+for i in /dcl01/lieber/ajaffe/psychENCODE_Data/USC_U01MH103346/BAM/*.bam
+    do echo $i
+    ## Figure out number of mapped reads
+    samtools view -c -F 4 $i
+done
+
+## Create .bam.bai files
+find /dcl01/lieber/ajaffe/psychENCODE_Data/USC_U01MH103346/BAM -name '*.bam' | parallel --jobs 10 samtools index
